8720c7b388e2672bc1142fe7c28ba54bddb6645e
F01000019 <chenkaijian@fineclouds.com>
2016-07-19 13:46:10 +0800

    modify

--- app/src/main/java/com/ckj/codedemo/Singleton.java
+++ app/src/main/java/com/ckj/codedemo/Singleton.java
@@ -10,7 +10,7 @@ public class Singleton {
     public static synchronized Singleton getInstance() {
         if (instance == null) {
             instance = new Singleton();
-            return instance;
         }
+        return instance;
     }
 }
